f596d58f7f64f6612bc5eba036fa851d3c7e864c
Kaixi26 <jorgejg575926@gmail.com>
2021-03-08 17:13:57 +0000

    better graph gen

--- PERFIL_SDC/SDLE/G00/main.py
+++ PERFIL_SDC/SDLE/G00/main.py
@@ -4,33 +4,36 @@ import networkx as nx
 from random import randrange
 import matplotlib.pyplot as plt
 
-size_list = [x for x in range(10,1001,10)]
-added_vertices_list = [0 for x in size_list]
-#print(size_list)
-#print(added_vertices_list)
-
-print("")
-for i in range(0, len(size_list)):
+def genConnected(size):
     G = nx.Graph()
-    size = size_list[i]
-
     for node in range(0, size):
         G.add_node(node)
-        
-    added_vertices = 0
     while not nx.is_connected(G):
         vi = randrange(size)
         vf = randrange(size)
         if vf not in G.adj[vi]:
             G.add_edge(vi, vf)
-            added_vertices = added_vertices + 1
+    return G
 
-    added_vertices_list[i] = added_vertices
-    print("\x1b[1F" + "Last size calculated: " + str(size))
+max_size = 100
+step = 10
+repetitions = 25
+
+size_list = [x for x in range(10,(max_size+1),step)]
+added_vertices_list = [0 for x in size_list]
+#print(size_list)
+#print(added_vertices_list)
+
+for i in range(0, len(size_list)):
+    total = 0
+    for j in range(0,repetitions):
+        total = total + len(genConnected(size_list[i]).edges)
+    added_vertices_list[i] = total/repetitions
+    print("\x1b[1F" + "Last size calculated: " + str(size_list[i]))
 
 #print("Size List:\t" + str(size_list))
 #print("Added vertices:\t" + str(added_vertices_list))
 
 plt.plot(size_list, added_vertices_list, 'ro')
-#plt.axis([0, 6, 0, 20])
+plt.plot(size_list, added_vertices_list)
 plt.show()
